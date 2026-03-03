e06dc9e63c574b55f9afec6ae1c3b90c4ad0b5aa
tijanadmi <tijanadmi@yahoo.com>
2026-03-03 12:32:23 +0100

    brisanje viska koda

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 mt-12 py-6 text-center text-sm text-pink-600">
-      © {new Date().getFullYear()} YogaLife · Sva prava zadržana.
-    </footer>
-  );
-}
